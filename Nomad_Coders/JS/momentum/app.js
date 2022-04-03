53fcf6d3b7711a35248155f2cece8608564128ca
lKori <yuy0841@gmail.com>
2022-04-03 11:48:04 +0900

    20220403_JS
    Nomad Corder JS Chrome App

---
+++ Nomad_Coders/JS/momentum/app.js
@@ -0,0 +1,17 @@
+const calculater = {
+    plus: function(a, b) {
+        console.log(a + b);
+    },
+    minus: function(a, b) {
+        console.log(a - b);
+    },
+    times: function(a, b) {
+        console.log(a * b);
+    },
+    divide: function(a, b) {
+        console.log(a / b);
+    },
+    power: function(a, b) {
+        console.log(a ** b);
+    },
+}
